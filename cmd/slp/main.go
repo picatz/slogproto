52ae53602b045ec8236e9024910e406c385e34b7
Kent 'picat' Gruber <kent.picat.gruber@gmail.com>
2024-08-09 00:22:17 -0400

    Refactor `slp` command using new changes

--- cmd/slp/main.go
+++ cmd/slp/main.go
@@ -8,54 +8,18 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
+	"os/signal"
 
 	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/checker/decls"
 	"github.com/picatz/slogproto"
-	"golang.org/x/exp/slog"
 )
 
-func filterExprProgram(filterExpr string) (cel.Program, error) {
-	// Create a CEL environment.
-	env, err := cel.NewEnv(
-		cel.Declarations(
-			decls.NewVar("msg", decls.String),
-			decls.NewVar("level", decls.Int),
-			decls.NewVar("time", decls.Timestamp),
-			decls.NewVar("attrs", decls.NewMapType(decls.String, decls.Any)),
-		),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("error creating CEL environment: %s", err)
-	}
-
-	// Parse the expression.
-	ast, iss := env.Compile(filterExpr)
-	if iss.Err() != nil {
-		return nil, fmt.Errorf("parse error: %s", iss.Err())
-	}
-
-	// Check the type of the expression.
-	checked, issues := env.Check(ast)
-	if issues != nil && issues.Err() != nil {
-		return nil, fmt.Errorf("type-check error: %s", issues.Err())
-	}
-
-	if checked.OutputType().DeclaredTypeName() != "bool" {
-		return nil, fmt.Errorf("invalid filter expression output type: %s", checked.OutputType().DeclaredTypeName())
-	}
-
-	// Return the program.
-	prog, err := env.Program(checked)
-	if err != nil {
-		return nil, fmt.Errorf("program construction error: %s", err)
-	}
-
-	return prog, nil
-}
-
 func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
 	var filterProg cel.Program
 
 	filterExpr := flag.String("filter", "", "filter expression")
@@ -65,7 +29,7 @@ func main() {
 	// If the filter is not empty, then compile it.
 	if filterExpr != nil && *filterExpr != "" {
 		var err error
-		filterProg, err = filterExprProgram(*filterExpr)
+		filterProg, err = slogproto.CompileFilter(*filterExpr)
 		if err != nil {
 			panic(fmt.Errorf("error compiling filter expression: %s", err))
 		}
@@ -103,37 +67,17 @@ func main() {
 
 	// Read the protobuf messages from the reader and write them to S
 	// TDOUT in JSON format.
-	err := slogproto.Read(context.Background(), r, func(r *slog.Record) bool {
-		if filterProg != nil {
-			attrsMap := make(map[string]any, r.NumAttrs())
+	err := slogproto.Read(ctx, r, func(r *slog.Record) bool {
+		include, err := slogproto.EvalFilter(filterProg, r)
+		if err != nil {
+			logger.Error("error evaluating filter expression", "expr", filterExpr, "error", err)
+			return false
+		}
 
-			r.Attrs(func(a slog.Attr) bool {
-				attrsMap[a.Key] = a.Value.Any()
-				return true
-			})
-
-			out, _, err := filterProg.Eval(map[string]any{
-				"msg":   r.Message,
-				"level": r.Level,
-				"time":  r.Time,
-				"attrs": attrsMap,
-			})
-			if err != nil {
-				panic(fmt.Errorf("error evaluating filter expression: %s", err))
-			}
-			v, ok := out.Value().(bool)
-			if !ok {
-				panic(fmt.Sprintf("invalid filter expr output value type: %T", out.Value()))
-			}
-			if !v {
-				return true
-			}
-			// Print the record.
-			logger.Handler().Handle(context.Background(), *r)
-		} else {
-			// Print the record.
+		if include {
 			logger.Handler().Handle(context.Background(), *r)
 		}
+
 		return true
 	})
 	if err != nil {
